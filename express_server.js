aca4ff1124b5871d0161b6f4a0ed53f3eb46ecd3
toddteigland <toddteigland@gmail.com>
2022-11-28 14:19:27 -0800

    Added a second page to views, and updated express to now take in value from the url bar to then take you to the show page, and show the long url for the short one entered

--- express_server.js
+++ express_server.js
@@ -4,15 +4,25 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-})
-
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { 
+    id: req.params.id, 
+    longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
