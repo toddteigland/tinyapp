7a8943fde585bd83f88bcb022641625761740e5e
toddteigland <toddteigland@gmail.com>
2022-11-28 15:41:57 -0800

    Added a new page that includes a form to submit long URLS, added a function to expressserver to convert to randmon 6 dig character and linked the two

--- express_server.js
+++ express_server.js
@@ -3,16 +3,30 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString() {
+  let RdmNum = (length=6)=>Math.random().toString(36).substr(2, length)
+  return RdmNum;
+}
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/:id", (req, res) => {
@@ -21,6 +35,8 @@ app.get("/urls/:id", (req, res) => {
     longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
+
+
 
 
 
