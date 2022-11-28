5b34695ac8edc4821d82c13a6eedacb4a5e38f39
toddteigland <toddteigland@gmail.com>
2022-11-28 13:42:41 -0800

    Added a views folder, then base html code to the urlsindex inside. Also added the link in express_server to accept the urlsindex link

--- express_server.js
+++ express_server.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+
 app.set("view engine", "ejs");
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+})
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
